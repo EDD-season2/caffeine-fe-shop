35656d8c4824658c73e562bbebacb5ee519bcae6
Jinwoo Shin <bluesky0255@gmail.com>
2019-12-12 16:40:29 +0900

    Integrate Response and Entity class

--- src/model/MenuItem.ts
+++ src/model/MenuItem.ts
@@ -2,12 +2,27 @@ export default class MenuItem {
     constructor (
         private _id: number,
         private _name: string,
-        private _desc: string,
-        private _price: number
+        private _nameInEnglish: string,
+        private _description: string,
+        private _price: number,
+        private _imgUrl: string
     ) {}
+
+    public static from (json: any): MenuItem {
+        return new MenuItem(
+            json.id,
+            json.name,
+            json.nameInEnglish,
+            json.description,
+            json.price,
+            json.imgUrl
+        )
+    }
 
     get id () { return this._id }
     get name () { return this._name }
-    get description () { return this._desc }
+    get nameInEnglish () { return this._nameInEnglish }
+    get description () { return this._description }
     get price () { return this._price }
+    get imgUrl () { return this._imgUrl }
 }
