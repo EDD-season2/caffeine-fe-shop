37b6e44f1fdd6d0459138395b80f33dd2ea0e371
Jinwoo Shin <bluesky0255@gmail.com>
2019-12-18 23:30:22 +0900

    Update how to check signed in in login needed view

--- src/views/LoginNeededView.ts
+++ src/views/LoginNeededView.ts
@@ -1,12 +1,12 @@
 import Vue from 'vue'
 import Component from 'vue-class-component'
-import Shop from '@/model/Shop'
+import Owner from '@/model/Owner'
 
 @Component
 export default class LoginNeededView extends Vue {
     protected async ensureSignedIn () {
         await this.$store.dispatch('refreshCurrentOwner')
-        if (this.$store.state.currentShop === Shop.UNAUTHENTICATED) {
+        if (this.$store.state.currentOwner === Owner.UNAUTHENTICATED) {
             this.$store.dispatch('receiveNotification', '로그인이 필요합니다')
             this.$router.push('/login')
         }
