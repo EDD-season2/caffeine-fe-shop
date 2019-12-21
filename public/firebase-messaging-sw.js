c2d26b1f2845e0ef0dcd74c209b810db412e63fc
Jinwoo Shin <bluesky0255@gmail.com>
2019-12-21 23:22:50 +0900

    Update service worker script version

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,13 +1,16 @@
 // Give the service worker access to Firebase Messaging.
 // Note that you can only use Firebase Messaging here, other Firebase libraries
 // are not available in the service worker.
-importScripts('https://www.gstatic.com/firebasejs/6.3.4/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/6.3.4/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/7.6.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/7.6.1/firebase-messaging.js');
 
 // Initialize the Firebase app in the service worker by passing in the
 // messagingSenderId.
 firebase.initializeApp({
-  'messagingSenderId': '92007077963'
+  apiKey: 'AIzaSyDZXTnA3yjOaZQ8JMajcAWiXtXpdoLK1OI',
+  projectId: 'caffeine-fe-shop',
+  messagingSenderId: '92007077963',
+  appId: '1:92007077963:web:495e3cd4ab6e927e4455ce'
 })
 
 // Retrieve an instance of Firebase Messaging so that it can handle background
